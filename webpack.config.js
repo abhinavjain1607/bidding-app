2620140186571fdb6550f446ef91ecc648894d4c
Abhinav Jain <abhinav.jain@knolskape.com>
2016-07-19 22:50:22 +0530

    webpack config for css

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var path = require('path');
 module.exports = {
 	context: __dirname + '/app',
 	entry: "./index.js",
@@ -7,13 +8,31 @@ module.exports = {
 	},
 	devtool: "source-map",
 	module: {
-		loaders: [{	
+		loaders: [{
 			test: /\.jsx?$/,
 			exclude: /(node_modules|bower_components)/,
-			loader: 'babel',
-			query: {
-				presets: ['es2015','react']
-			}
+			loaders: ['babel?presets[]=es2015,presets[]=react,presets[]=survivejs-kanban']
+		}, {
+			test: /\.sass$/,
+			loaders: ['style?sourceMap', 'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]', 'sass?sourceMap']
+		}, {
+			test: /\.css$/,
+			loader: 'style!css?sourceMap'
+		}, {
+			test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+			loader: "url?limit=10000&mimetype=application/font-woff"
+		}, {
+			test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+			loader: "url?limit=10000&mimetype=application/font-woff"
+		}, {
+			test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+			loader: "url?limit=10000&mimetype=application/octet-stream"
+		}, {
+			test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+			loader: "file"
+		}, {
+			test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+			loader: "url?limit=10000&mimetype=image/svg+xml"
 		}]
 	},
 	resolve: {
